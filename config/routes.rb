574f3072234d447358ef05af5d75bcbb734f86b3
toku-kou8 <koichi.university@outlook.jp>
2021-03-30 07:36:06 +0000

    finish4

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   get 'users/show'
   root to: 'homes#top'
   get "home/about" => "homes#about"
-  
+  delete "/books/:book_id/favorites" => "favorites#destroy", as: 'book_favorites'
+  post "/books/:book_id/favorites" => "favorites#create", as: 'book_favorite'
   devise_for :users
   
-  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
+    resources :book_comments, only: [:create, :destroy]
+  end
   
   resources :users, only: [:show, :edit, :update, :index]
 
